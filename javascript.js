376c08485a9c1fe36023058a4bccad1c9b927971
ablaede <blaed026@umn.edu>
2025-10-15 19:50:09 -0500

    copied multiplication table code from Nik repo

--- javascript.js
+++ javascript.js
@@ -1,89 +1,33 @@
-function getrowsdesired() {
-    /*console.log(document.getElementById('rows').value);*/
-    return Number(document.getElementById('rows').value);
+function getRowsDesired() {
+    return parseInt(document.getElementById("rows").value)
 }
 
-function getcolumnsdesired() {
-    /*console.log(document.getElementById('cols').value);*/
-    return Number(document.getElementById('cols').value);
+function getColumnsDesired() {
+    return parseInt(document.getElementById("cols").value)
 }
 
 function createWithFlexbox(rows, cols) {
-    let string
-    if (rows && cols) {
-        string = "<table>\n"
+    let string = ''
 
-        for (let row =1; row <= rows; row++) {
+    // first do-while loop to build the flex'd rows
+    let row = 1
+    do {
+        string += "<div class='row'>"
+        string += row
 
-            if (highlight && highlight ===row) {
-                string += "<tr class='highlight'>\n"
-            } else {
-                string += "<tr>\n"
-            }
+        // second do-while loop to build the flex'd columns
+        let col = 1
+        do {
+            string += "<div class='col'>"
+            string += row * col
+            string += "</div>"
 
-            for (let col =1; col <= cols; col++) {
-                string += "<td>"
-                string += row * col
-                string += "</td>\n"
-            }
-            string += "</tr>\n"
-        }
-        string += "</table>\n"
-    } else {
-        string = 'Provide some inputs'
-    }
-    document.getElementById("output").innerHTML = string;
+            col = col + 1
+        } while (col <= cols)
+        string += "</div>\n"
+
+        row = row + 1;
+    } while (row <= rows)
+
+    document.getElementById("output").innerHTML = string
 }
-function gethihglightvalue() {
-    let highlightvalue = document.getElementById('highlight').value;
-
-    if (!isNaN(highlightvalue !== '')) {
-        return Number(highlightvalue);
-    }
-}
-//
-/* letters are irrelevant but we usually use i and j*/
-
-/* console.log('Here\'s a for loop showing 1...10:\n')
-for (let i = 0; i < 10; i++) {
-    console.log(`${ i }\n`)
-}
-
-console.log('Here\'s a do-while loop showing 1...10:\n')
-let i = 0
-do {
-    console.log(`${ i }\n`)
-    i++
-} while (i < 10)
-
-console.log('Here\'s a while loop showing 1...10:\n')
-let j = 0
-while (j < 10) {
-    console.log(`${ j }\n`)
-    j++
-}
-
-console.log('Here\'s a foreach loop iterating over an array showing 1...10:\n')
-let nums=[...Array(100).keys()]
-nums.forEach(num =>{
-    console.log(`${ num }\n`)
-})
-
-console.log('Here\'s a multiplication table method rendered in the console with a for loop:\n')
-console.log('This demos variables, concatenation, for loops, console.logging and methods, along with method paragraph')
-let timestable = (to) => {
-    console.log('Here\'s a times table for to ' + to + '\n')
-
-    if (Number.isInteger(to)) {
-        for (let i = 1; i <= to; i++) {
-            let row = ""
-            for (let j = 1; j <= to; j++) {
-                row += `${i * j}\t`
-            }
-            console.log(row)
-        }
-    } else {
-        console.log(`sorry, ${to} isn't a number!`)
-    }
-}
-timestable(12) */
