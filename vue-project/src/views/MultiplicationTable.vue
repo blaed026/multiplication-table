0eeec7167d1582f2fb59de935167e4018bd028d5
ablaede <blaed026@umn.edu>
2025-10-20 16:56:34 -0500

    stood up multiplication table project, built initial routing, and WIP on the page itself

--- vue-project/src/views/MultiplicationTable.vue
+++ vue-project/src/views/MultiplicationTable.vue
@@ -1,12 +1,57 @@
-<script>
+<script setup lang="ts"> // for Javascript //
 
+import {ref} from 'vue';
+
+let name = ref(null)
+let rows = ref(12)
+let cols = ref(12)
+
+console.log( 'How many rows do I have?', rows.value)
 </script>
 
-<template>
-  <h1>hello world</h1>
-  <p>multiplication table lives here</p>
+<template> <!-- Html Mark up would go here -->
+  <h1>My multiplication table will live here!</h1>
+
+  <div class="inputs">
+
+    <label for="name">
+      <span>Whats your name:</span>
+      <input id="name" v-model="name"/>
+    </label>
+
+    <label for="rows">
+      <span>Rows</span>
+      <input type="number" id="rows" v-model.number="rows"/>
+    </label>
+
+    <label for="cols">
+      <span>Cols</span>
+      <input type="number" id="cols" v-model.number="cols"/>
+    </label>
+
+    <p v-if="name"> Hi there {{name}}, lets build our table:</p>
+    <p v-else>Hi there!</p>
+
+    <p>How many rows do I have? {{rows}}</p>
+    <p>How many cols do I have? {{cols}}</p>
+  </div>
 </template>
 
-<style scoped>
+<style scoped> /* CSS code would go here, JUST THIS DOC */
+div.inputs {
+  width: 50%;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
 
+  label {
+    padding: 10px;
+    border: 1px solid red;
+    display: flex;
+    gap: 15px;
+  }
+}
 </style>
+
+
